5ba835757a143dd01a6a7153b205811663f33f1e
N00234646@iadt.ie <N00234646@iadt.ie>
2025-02-22 12:34:46 +0000

    Creating menu for all charts visualization. Working commit for mouse pressed function and buttons. Errors prevalent

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let data;
 let cleanedData = [];
 let charts = [];
 let customfont;
+let state = "menu"; 
+let currentChartIndex = 0; 
 
 function preload() {
     data = loadTable('data/Combined.csv', 'csv', 'header');
@@ -9,11 +11,38 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(5000, 4000); 
+    createCanvas(5000, 4000);
     angleMode(RADIANS);
-    noLoop();
+    
     cleanData();
+    setupCharts();
+}
 
+function draw() {
+    background(0, 38, 77); 
+
+    if (state === "menu") {
+        drawMenu();
+    } else if (state === "visualization") {
+        drawVisualization();
+    }
+}
+
+
+function cleanData() {
+    for (let i = 0; i < data.rows.length; i++) {
+        cleanedData.push(data.rows[i].obj);
+    }
+
+    for (let i = 0; i < cleanedData.length; i++) {
+        cleanedData[i].Domestic = parseInt(parseInt(cleanedData[i]["Domestic (USD)"]) / 1000000);
+        cleanedData[i].International = cleanedData[i]["International (USD)"] === "Unknown" ? 
+            0 : parseInt(parseInt(cleanedData[i]["International (USD)"]) / 1000000);
+        cleanedData[i].Total = parseInt(parseInt(cleanedData[i]["Total Worldwide (USD)"]) / 1000000);
+    }
+}
+
+function setupCharts() {
     charts.push(new BarChart({
         data: cleanedData,
         xValue: "Title",
@@ -44,8 +73,8 @@ function setup() {
         barWidth: 40,
         margin: 15,
         axisThickness: 3,
-        chartPosX: 2000,
-        chartPosY: 1050,
+        chartPosX: 1050,
+        chartPosY: 200,
         customFont: customfont,
         title: "Horizontal Bar Chart - International Box Office",
         xAxisTitle: "Box Office (In Millions USD)",
@@ -64,8 +93,8 @@ function setup() {
         barWidth: 40,
         margin: 15,
         axisThickness: 3,
-        chartPosX: 200,
-        chartPosY: 650,
+        chartPosX: 1050,
+        chartPosY: 200,
         customFont: customfont,
         title: "Vertical Bar Chart - Domestic Box Office",
         xAxisTitle: "Movie Title",
@@ -84,8 +113,8 @@ function setup() {
         barWidth: 40,
         margin: 15,
         axisThickness: 3,
-        chartPosX: 200,
-        chartPosY: 1500,
+        chartPosX: 1050,
+        chartPosY: 200,
         customFont: customfont,
         title: "Vertical Bar Chart - International Box Office",
         xAxisTitle: "Movie Title",
@@ -104,8 +133,8 @@ function setup() {
         barWidth: 40,
         margin: 15,
         axisThickness: 3,
-        chartPosX: 2000,
-        chartPosY: 650,
+        chartPosX: 1050,
+        chartPosY: 200,
         customFont: customfont,
         title: "Stacked Bar Chart - Box Office",
         xAxisTitle: "Movie Title",
@@ -125,7 +154,7 @@ function setup() {
         margin: 15,
         axisThickness: 3,
         chartPosX: 1050,
-        chartPosY: 1500,
+        chartPosY: 200,
         customFont: customfont,
         title: "100% Stacked Bar Chart - Box Office",
         xAxisTitle: "Movie Title",
@@ -143,8 +172,8 @@ function setup() {
         chartWidth: 600,
         margin: 15,
         axisThickness: 3,
-        chartPosX: 200,
-        chartPosY: 2500,
+        chartPosX: 1050,
+        chartPosY: 200,
         customFont: customfont,
         title: "Linear Regression - Domestic Box Office",
         xAxisTitle: "Year",
@@ -162,31 +191,13 @@ function setup() {
         chartWidth: 600,
         margin: 15,
         axisThickness: 3,
-        chartPosX: 1050, 
-        chartPosY: 2000,
+        chartPosX: 1050,
+        chartPosY: 200,
         customFont: customfont,
         title: "Spider Plot - Box Office",
         xAxisTitle: "Movies",
         yAxisTitle: "Box Office (In Millions USD)",
-        barColours: [color(255, 215, 0), color(0, 191, 255)], 
+        barColours: [color(255, 215, 0), color(0, 191, 255)],
         showAverageLine: false
     }));
-}
-
-function draw() {
-    background(0, 38, 77);
-    charts.forEach(chart => chart.render());
-}
-
-function cleanData() {
-    for (let i = 0; i < data.rows.length; i++) {
-        cleanedData.push(data.rows[i].obj);
-    }
-
-    for (let i = 0; i < cleanedData.length; i++) {
-        cleanedData[i].Domestic = parseInt(parseInt(cleanedData[i]["Domestic (USD)"]) / 1000000);
-        cleanedData[i].International = cleanedData[i]["International (USD)"] === "Unknown" ? 
-            0 : parseInt(parseInt(cleanedData[i]["International (USD)"]) / 1000000);
-        cleanedData[i].Total = parseInt(parseInt(cleanedData[i]["Total Worldwide (USD)"]) / 1000000);
-    }
 }
